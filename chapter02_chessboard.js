6287226e23ab93aaf4877e064f94758b3e528d67
Sebastianbc <sebastianbrinezc@gmail.com>
2022-04-06 10:13:28 -0500

    Refactor code to implement ternary operators in if/else statements

--- chapter02_chessboard.js
+++ chapter02_chessboard.js
@@ -1,15 +1,11 @@
 let number = 1, chessboard = "", size = 8
 do {
   // If current position is even, put a sharp sign, when it isn't put a space
-  if (number % 2 === 0) {
-    chessboard += "#"
-  } else {
-    chessboard += " "
-  }
+  chessboard += number % 2 === 0 ? "#" : " "
+
   // When reach the 8th position in a row, do a line jump
-  if (chessboard.length % 8 === 0) {
-    chessboard += "\n"
-  }
+  chessboard += chessboard.length % size === 0 ? "\n" : ""
+  
   // Increment position by one
   number++
 } while (number <= (size * size))
