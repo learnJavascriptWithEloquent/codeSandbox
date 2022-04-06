0d7739536fd43dd410fd8a26a46b65f42b960fe9
Johann Brinez Correa <brinezcorrea.6@teleperformance.co>
2022-04-06 08:40:44 -0500

    Third exercise from Elouquen JS chapter 02

---
+++ chapter02_chessboard.js
@@ -0,0 +1,17 @@
+let number = 1, chessboard = "", size = 8
+do {
+  // If current position is even, put a sharp sign, when it isn't put a space
+  if (number % 2 === 0) {
+    chessboard += "#"
+  } else {
+    chessboard += " "
+  }
+  // When reach the 8th position in a row, do a line jump
+  if (chessboard.length % 8 === 0) {
+    chessboard += "\n"
+  }
+  // Increment position by one
+  number++
+} while (number <= (size * size))
+
+console.log(chessboard)
